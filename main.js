a9882c7a9d74084ccc5602589f3a70842d691577
Femi-code <66394869+Femi-code@users.noreply.github.com>
2020-07-29 21:33:54 +0100

    Added my project

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let thumbnails = document.getElementsByClassName('thumbmail');
+let activeImages = document.getElementsByClassName('active');
+
+for(var i=0;i<thumbnails.length;i++){
+	thumbnails[i].addEventListener('mouseover', function(){
+        if(activeImages.length>0){
+		     activeImages[0].classList.remove('active');
+	    }
+        this.classList.add('active');
+        document.getElementById('featured').src = this.src
+	})
+}
+
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+
+buttonLeft.addEventListener('click', function(){
+	document.getElementById('slider').scrollLeft -= 180
+})
+
+buttonRight.addEventListener('click', function(){
+	document.getElementById('slider').scrollLeft += 180
+})
